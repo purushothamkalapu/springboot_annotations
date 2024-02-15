26d2b1dc4244d175afe9584755096957d6f45d54
Purushotham <119475738+purushothamkalapu@users.noreply.github.com>
2024-02-15 10:08:34 +0530

    -> @Autowared annotation is used to inject the bean automatically -> @autowared annotation is used in constructor injection, setter injection, and field injection

--- src/main/java/com/purushotham/spring_annotations/controller/PizzaController.java
+++ src/main/java/com/purushotham/spring_annotations/controller/PizzaController.java
@@ -1,11 +1,29 @@
 package com.purushotham.spring_annotations.controller;
 
+import com.purushotham.spring_annotations.service.VegPizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Component("pizzaDemo")
 @Component()
 public class PizzaController {
+    @Autowired
+    private VegPizza vegPizza;
+    /*Constructor injection*/
+
+    /*@Autowired
+    public PizzaController(VegPizza vegPizza) {
+        this.vegPizza = vegPizza;
+    }*/
+
+    /*Setter injection*/
+    /*@Autowired
+    public void setVegPizza(VegPizza vegPizza) {
+        this.vegPizza = vegPizza;
+    }
+*/
+
     public String getPizza(){
-        return "Hot Pizza";
+        return vegPizza.getPizza();
     }
 }
---
+++ src/main/java/com/purushotham/spring_annotations/service/VegPizza.java
@@ -0,0 +1,10 @@
+package com.purushotham.spring_annotations.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VegPizza {
+    public String getPizza(){
+        return "Vez Pizza";
+    }
+}
